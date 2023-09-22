43060ce422fa8c30df51255084d4558f136e7d96
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-22 20:14:57 -0300

    feat: add delete images

---
+++ src/firebase/deleteFile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { deleteObject, ref } from 'firebase/storage';
+import { storage } from './config';
+
+const deleteFile = (filePath) => {
+    const imageRef = ref(storage, filePath);
+  return deleteObject(imageRef);
+};
+
+export default deleteFile;
